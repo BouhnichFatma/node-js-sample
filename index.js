57722138a50795be7123284e6a18b1699eadc5e2
BouhnichFatma <73854154+BouhnichFatma@users.noreply.github.com>
2021-03-10 13:03:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project')
 }) 
-
+const host = '0.0.0.0';
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
