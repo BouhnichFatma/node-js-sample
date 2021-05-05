b5c360dbc2fc717f390b450e865b0a82d6ba0197
BouhnichFatma <73854154+BouhnichFatma@users.noreply.github.com>
2021-05-05 11:43:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello !!!')
+  response.send('Hello Testing DevOps Node! JS Project Hello')
 }) 
 const host = '0.0.0.0';
 app.listen(app.get('port'), function() {
